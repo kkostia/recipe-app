9f6ad7730e999b0dee789e2902b581749be3368c
Kostiantyn Panasenko <g00436001@atu.ie>
2025-04-23 20:21:14 +0100

    added a bit of styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [IonicModule, RouterOutlet, RouterModule], // all the imports we need
+  imports: [IonicModule, RouterOutlet, RouterModule, CommonModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() { }
+}
